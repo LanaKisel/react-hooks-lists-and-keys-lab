e1014990eb1a8e78fe7e9b95520d17e7a6e5c144
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-11-24 19:16:39 -0800

    adds keys to the arrays

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+
+  const newProjects= projects.map((project)=>{ return <ProjectItem about={project.about} key={project.id} name={project.name} title={project.title} technologies={project.technologies}/>}
+  )
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{newProjects}</div>
     </div>
   );
 }
